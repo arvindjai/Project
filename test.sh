a8057c1d74350310dceacb95811b23fbcb107db1
Arvind Jaiswal <26666567+arvindjai@users.noreply.github.com>
2025-02-03 17:20:02 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "Hello, Testing Git webhook..."
-
+echo "Date ..."
 date
+echo "Display PWD"
 pwd
+echo "List content here..."
 ls -al
+echo "Disk Free"
 df -h
+echo "List block size.."
 lsblk
